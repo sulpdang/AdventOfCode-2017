5adf4050eed8182ad4f758abd0d6af55312c4a53
Yohho Kim <sulpdang@gmail.com>
2017-12-06 04:33:09 +0900

    Solve day4

---
+++ day4/src/main/scala/Main.scala
@@ -0,0 +1,28 @@
+//
+// Main
+//
+// Copyright (c) 2017 Yohho (sulpdang@gmail.com)
+//
+
+object Main extends App {
+  type Word = String
+  type Words = Array[Word]
+  def readContent:Array[Words] = {
+    scala.io.Source.fromResource("input.txt")
+      .mkString
+      .split('\n')
+      .map{_.split(' ').toArray}
+  }
+  def solve(wordsContainer:Array[Words]):Int = {
+    wordsContainer.count{words => 
+      words.toSet.size == words.size
+    }
+  }
+  def solve2(wordsContainer:Array[Words]):Int = {
+    wordsContainer.count { words => 
+      words.map{_.sorted}.toSet.size == words.size
+    }
+  }
+  println(solve(readContent))
+  println(solve2(readContent))
+}
