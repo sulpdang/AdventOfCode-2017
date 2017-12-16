c05d8c9f3a0e7fe42b7e89053d799a52d300245b
Yohho Kim <sulpdang@gmail.com>
2017-12-16 11:09:05 +0900

    Make more Functional way

--- day14/src/main/scala/Main.scala
+++ day14/src/main/scala/Main.scala
@@ -5,90 +5,64 @@
 //
 
 import myutil._
+import scala.language.implicitConversions
 
 object Main extends Day(14) {
 
   import KnotHash._
 
   type Input = String
+  case class Pos(x:Int, y:Int) {
+    def +(other:Pos) = Pos(x + other.x, y + other.y)
+  }
+
+  implicit class IntUtil(val ivalue:Int) extends AnyVal {
+    def toBinaryPadString(num:Int, padStr:String) = {
+      (padStr * num + ivalue.toBinaryString).takeRight(num)
+    }
+  }
 
   def processedInput = input.head
 
   def makeMap(input:Input) = {
     val hexString = "0123456789abcdef"
-    (0 to 127)
-      .map{i => knotHash(s"${input}-$i")
-        .flatMap{ hexString.indexOf(_).toBinaryString.reverse.padTo(4,"0").reverse }
-        .mkString("")
-        .toCharArray
-      }.toArray
+    for(i <- (0 to 127).toList) yield {
+      (for{
+        c   <- knotHash(s"${input}-$i")
+        bit <- hexString.indexOf(c).toBinaryPadString(4,"0")
+      } yield bit).mkString("")
+    }
   }
 
-  def solve(input:Input)  = makeMap(input).map{_.count{_ == '1'}}.sum
+  def solve(input:Input) = makeMap(input).map{_.count{_ == '1'}}.sum
 
   def solve2(input:Input) = {
-    val map = makeMap(input)
+    val map = makeMap(input).map{_.toArray}
+
     val direction = List((1,0), (-1,0), (0,1), (0,-1))
+
     def dfs(x:Int, y:Int) {
       def isPossible(x:Int) = 0 <= x && x <= 127
-      map(x)(y) match {
-        case '1' =>  {
+      if(map(x)(y) == '1') {
           map(x)(y) = '0'
           direction
-            .filter {case (a,b) => isPossible(a+x) && isPossible(b+y)}
-            .foreach{case (a,b) => dfs(a+x, b+y) }
-        }
-        case _ =>
+            .map    {case (i,j) => (i+x, j+y)}
+            .filter {case (i,j) => isPossible(i) && isPossible(j)}
+            .filter {case (i,j) => map(i)(j) == '1'}
+            .foreach{case (i,j) => dfs(i, j) }
       }
     }
 
-    var t = 0
-    for{
+    (for{
       i <- (0 until 128)
       j <- (0 until 128)
-      if map(i)(j) == '1'
-    } {
-      dfs(i, j)
-      t += 1
-    }
-    t
+    } yield i -> j)
+      .iterator
+      .foldLeft(0){ 
+        case (acc, (i, j)) if map(i)(j) == '1' => dfs(i, j); acc + 1
+        case (acc, _) => acc
+      }
   }
-
 }
 
 
-object KnotHash {
-
-  def reverseArray(arr:Array[Int], s:Int, l:Int, size:Int) = {
-    for{
-      i <- (0 until l)
-      end = ((l - 1 - i + s) % size)
-      curPos = (i + s) % size
-      if curPos <= end
-    } {
-      val temp = arr(end)
-      arr(end) = arr(curPos) ; arr(curPos) = temp
-    }
-  }
-
-  def roundHash( arr:Array[Int], lengths:List[Int], curPos:Int, skip:Int) :(Int, Int) = {
-      val size = arr.size
-      lengths.foldLeft((curPos, skip)) { case ((startPos, skip), length) => {
-        reverseArray(arr, startPos, length, size)
-        ((startPos + length + skip) % size, skip+1)
-      }}
-  }
-
-  def knotHash(inputString:String) = {
-    val lengths = inputString.toCharArray.map{_.toInt}.toList ++ List(17,31,73,47,23)
-    val array = (0 to 255).toArray
-    (1 to 64).foldLeft((0, 0)){ case ((curPos, skip),_) =>
-      roundHash(array, lengths, curPos, skip)
-    }
-    array.grouped(16)
-      .map{_.reduce(_^_)}
-      .map{"%02x" format _}
-      .flatMap{_.toCharArray}.toList
-  }
-
-}
