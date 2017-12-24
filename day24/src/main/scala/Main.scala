af116e0db5b1890f62b7fb120a7391619527257b
Yohho Kim <sulpdang@gmail.com>
2017-12-24 17:03:01 +0900

    Day 24: Electromagnetic Moat(Solved)

---
+++ day24/src/main/scala/Main.scala
@@ -0,0 +1,55 @@
+//
+// Main
+//
+// Copyright (c) 2017 Yohho (sulpdang@gmail.com)
+//
+
+import myutil._
+import scala.collection._
+import myutil._
+
+case class Component(a:Int, b:Int) {
+  def contains(c:Int) = a == c || b == c
+  def getNext(c:Int) = if(a == c) b else a
+}
+
+case class Result(l:Int, m:Int)
+
+object Main extends Day(24) {
+  type Input  = List[Component]
+
+  def processedInput = input
+    .map{_.split('/').map{_.toInt}.sorted}
+    .map{case Array(a,b)=> Component(a.toInt, b.toInt)}
+
+
+  def traverseChild(id:Int, remains:Set[Component], max:Int=0, length:Int = 0)
+  (implicit order:Ordering[Result]):Result = {
+    if(remains.isEmpty) Result(length, max)
+    else {
+      val connected = remains.filter{_ contains id}
+      if(connected.isEmpty) Result(length, max)
+      else {
+        connected.map{ x =>
+          val nextID = x.getNext(id)
+          traverseChild(nextID, remains - x, max + nextID + id, length+1)
+        }.max
+      }
+    }
+  }
+
+  def solve(input:Input) = {
+    implicit def order:Ordering[Result] = new Ordering[Result] {
+      def compare(x:Result, y:Result) = x.m - y.m
+    }
+    traverseChild(0, input.toSet)
+  }
+
+  def solve2(input:Input) = {
+    implicit def order:Ordering[Result] = new Ordering[Result] {
+      def compare(x:Result, y:Result) = if(x.l != y.l) x.l - y.l else x.m - y.m
+    }
+    traverseChild(0, input.toSet)
+  }
+
+}
